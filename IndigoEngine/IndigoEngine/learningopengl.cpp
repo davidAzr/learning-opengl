da6c99f31ac70e4d93726ca8bc3cec334d420aaa
David Azuar Alonso <daa32@alu.ua.es>
2020-05-22 14:33:30 +0200

    Completed Camera section without camera class

--- IndigoEngine/IndigoEngine/learningopengl.cpp
+++ IndigoEngine/IndigoEngine/learningopengl.cpp
@@ -16,8 +16,21 @@
 #define FRAGMENT_SHADER_FILEPATH "shaders/fs_twotextures.glsl" 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void processInput(GLFWwindow* window);
 
+
+
+glm::vec3 cameraPos(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp(0.0f, 1.0f, 0.0f);
+const float movementSpeed = 2.f;
+
+float lastFrame;
+float deltaTime;
+
+float lastX, lastY, yaw = 0.f, pitch = 0.f, sensitivity = 0.1f;
+bool firstMouse = true;
 
 int main() {
 	// Inicializamos glfw
@@ -48,8 +61,11 @@ int main() {
 
 	glEnable(GL_DEPTH_TEST);
 
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	
+	glfwSetCursorPosCallback(window, mouse_callback);
+
 	Shader shaderProgram(VERTEX_SHADER_FILEPATH, FRAGMENT_SHADER_FILEPATH);
 	shaderProgram.Use();
 
@@ -191,9 +207,23 @@ int main() {
 	glm::mat4 projection;
 	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
+	// Camera axis computation
+	//glm::vec3 cameraPos(0.f, 0.f, 3.f);
+	//glm::vec3 cameraTarget(0.f, 0.f, 0.f);
+	//glm::vec3 cameraDirection(cameraPos - cameraTarget);
+
+	//glm::vec3 up(0.f, 1.f, 0.f);
+	//glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+	//glm::vec3 cameraUp = glm::normalize(glm::cross(cameraDirection, cameraRight));
+
 
 	// Render loop
 	while (!glfwWindowShouldClose(window)) {
+
+		auto currentTime = glfwGetTime();
+		deltaTime = currentTime - lastFrame;
+		lastFrame = currentTime;
+
 		// input
 		processInput(window);
 
@@ -208,8 +238,11 @@ int main() {
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
-		glm::mat4 view(1.f);
-		view = glm::translate(view, glm::vec3(0.f, 0.f, -3.f));
+		const float radius = 10.f;
+		float camX = sin(glfwGetTime()) * radius;
+		float camZ = cos(glfwGetTime()) * radius;
+		glm::mat4 view;
+		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
 		glm::mat4 projection;
 		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
@@ -243,8 +276,52 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 }
 
 void processInput(GLFWwindow* window) {
+	auto cameraSpeed = movementSpeed * deltaTime;
+
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 	if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS)
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+		cameraPos += cameraSpeed * cameraFront;
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		cameraPos -= cameraSpeed * cameraFront;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		cameraPos -= cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		cameraPos += cameraSpeed * glm::normalize(glm::cross(cameraFront, cameraUp));
+	}
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+	
+	auto yOffset = lastY - ypos;
+	auto xOffset = xpos - lastX;
+	lastY = ypos;
+	lastX = xpos;
+
+	pitch += sensitivity * yOffset;
+	yaw += sensitivity * xOffset;
+
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 direction;
+	direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	direction.y = sin(glm::radians(pitch));
+	direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+
+	cameraFront = glm::normalize(direction);
 }
